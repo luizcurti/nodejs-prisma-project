d802aef3a33a2cb8abe257835b2994054224f784
Luiz Curti <luiz_curti@hotmail.com>
2025-04-04 13:25:04 +0100

    Update project

--- src/modules/deliveries/useCase/createDelivery/CreateDeliveryUseCase.ts
+++ src/modules/deliveries/useCase/createDelivery/CreateDeliveryUseCase.ts
@@ -7,10 +7,20 @@ interface ICreateDelivery {
 
 export class CreateDeliveryUseCase {
   async execute({ item_name, id_client }: ICreateDelivery) {
+    const deliverymen = await prisma.deliveryman.findMany();
+
+    if (deliverymen.length === 0) {
+      throw new Error("No available deliveryman found.");
+    }
+
+    const randomIndex = Math.floor(Math.random() * deliverymen.length);
+    const randomDeliveryman = deliverymen[randomIndex];
+
     const delivery = await prisma.deliveries.create({
       data: {
         item_name,
         id_client,
+        id_deliveryman: randomDeliveryman.id
       },
     });
 
--- src/modules/deliveries/useCase/updateDeliveryman/useCases/UpdateDeliverymanController.ts
+++ src/modules/deliveries/useCase/updateDeliveryman/useCases/UpdateDeliverymanController.ts
@@ -6,6 +6,9 @@ export class UpdateDeliverymanController {
     const { id_deliveryman } = request;
     const { id: id_delivery } = request.params;
 
+    console.log('1', id_deliveryman);
+    console.log('2', id_delivery);
+
     const updateDeliverymanUseCase = new UpdateDeliverymanUseCase();
     const delivery = await updateDeliverymanUseCase.execute({
       id_deliveryman,
